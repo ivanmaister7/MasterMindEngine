642f638841ea61f8896400baba9c5a40bb94bed2
Ivan Maister <ivanmaister7@gmail.com>
2022-12-11 17:22:45 +0200

    Fix tests

--- Tests/MasterMindEngineTests/GameSettingsTests.swift
+++ Tests/MasterMindEngineTests/GameSettingsTests.swift
@@ -4,17 +4,17 @@
     final class GameSettingsTests: XCTestCase {
 
         func test_prepareGameSettings_Ok() {
-            let settings = GameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            let settings = GameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtimeMode: true)
             var settings2 = GameSettings()
-            settings2.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            settings2.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtimeMode: true)
             
             XCTAssertTrue(settings == settings2)
         }
 
         func test_prepareGameSettings_Bad() {
-            let settings = GameSettings(rowSize: 4, moves: 8, colors: 6, duration: 15.0)
+            let settings = GameSettings(rowSize: 4, moves: 8, colors: 6, duration: 15.0, realtimeMode: true)
             var settings2 = GameSettings()
-            settings2.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            settings2.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtimeMode: true)
             
             XCTAssertTrue(settings != settings2)
         }
--- Tests/MasterMindEngineTests/MasterMindEngineTests.swift
+++ Tests/MasterMindEngineTests/MasterMindEngineTests.swift
@@ -4,23 +4,23 @@
     final class MasterMindEngineTests: XCTestCase {
         
         func test_initWithParams_Ok() {
-            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
-            let settings = GameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtime: true)
+            let settings = GameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtimeMode: true)
             
             XCTAssertTrue(settings == game.gameSettings)
         }
         
         func test_initWithParams_Bad() {
-            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
-            let settings = GameSettings(rowSize: 4, moves: 8, colors: 6, duration: 15.0)
+            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtime: true)
+            let settings = GameSettings(rowSize: 4, moves: 8, colors: 6, duration: 15.0, realtimeMode: true)
             
             XCTAssertTrue(settings != game.gameSettings)
         }
 
         func test_prepareGameSettings_and_initWithParams_Ok() {
-            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            let game = MasterMindEngine(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtime: true)
             var settings = GameSettings()
-            settings.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0)
+            settings.prepareGameSettings(rowSize: 5, moves: 7, colors: 5, duration: 15.0, realtimeMode: true)
             
             XCTAssertTrue(settings == game.gameSettings)
         }
